0171f35ac74175470075b9b19fba1f09bede577c
git\RakibAyon <a>
2021-06-22 14:42:44 -0500

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send({'hi':'there, port number' + `${process.env.PORT}`});
+    res.send({'hi':'there, port number ' + `${process.env.PORT}`});
 });
 
 app.get('/home',(req,res) => {
