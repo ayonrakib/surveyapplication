a55970b5d47ca3a1fb8d40e835657d4820764598
git\RakibAyon <a>
2021-07-01 09:43:18 -0500

    Passport

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const keys = require('./config/keys.js');
 
-app.get('/',(req,res) => {
-    res.send({'hi':'there, port number ' + `${process.env.PORT}`});
-});
-
-app.get('/home',(req,res) => {
-    res.send({name:'Scarecrow'});
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
