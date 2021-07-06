8e593eece2fc533cbdd894bd809a5e8e6173b74a
git\RakibAyon <a>
2021-07-06 11:37:05 -0500

    Indexing

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const app = express();
-const keys = require('./config/keys.js');
 
-passport.use(new GoogleStrategy());
+const app = express();
+
+
+
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
